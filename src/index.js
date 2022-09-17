858063630933583d5d50cf4921c28fb764107001
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-18 02:27:44 +0530

    tried using socket.io

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const blogRoute = require("./Routes/blogs.router");
 const cors = require("cors");
 const userRoute = require("./Routes/user.router");
 const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
 
 const app = express();
 app.use(cors());
@@ -18,7 +19,16 @@ app.use("/blogs", blogRoute);
 app.use("/users", userRoute);
 
 mongoose.connect(`mongodb://localhost:27017/myBlogs`).then(() => {
-	app.listen("8080", () => {
+	const server = app.listen("8080", () => {
 		console.log("DB connected, now listening on http://localhost:8080/");
 	});
+	const io = new Server(server, {
+		cors: {
+			origin: "http://localhost:5173",
+		},
+	});
+	io.on("connection", (socket) => {
+		console.log("new client connected");
+	});
+	app.set("socketio", io);
 });
